6f4c76c4ca94741add1d9c881ac68fbeb9e66263
Valera <yaganov2005v@mail.ru>
2026-06-29 14:06:08 +0400

    chore(server): initialize Spring Boot SOAP server

---
+++ soap-messenger-server/src/main/java/ru/ulstu/soapmessenger/SoapMessengerServerApplication.java
@@ -0,0 +1,13 @@
+package ru.ulstu.soapmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapMessengerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapMessengerServerApplication.class, args);
+	}
+
+}
---
+++ soap-messenger-server/src/test/java/ru/ulstu/soapmessenger/SoapMessengerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ulstu.soapmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapMessengerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
